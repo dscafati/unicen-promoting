21b4501830d88d3b59aa1e3a730f802489abb868
Diego Scafati <dscafati@gmail.com>
2016-07-15 00:53:27 -0300

    Added java project

---
+++ src/main/java/com/unicen/app/App.java
@@ -0,0 +1,14 @@
+package com.unicen.app;
+
+import com.informix.asf.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
